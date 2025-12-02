b05dead2b0b9de592ac9afab48e099c2cd640ca8
Antonio Cordero <antoniocorderomn@gmail.com>
2025-12-02 23:12:51 +0100

    Funcional - Falta mejorar estilos

--- routes/users.js
+++ routes/users.js
@@ -7,9 +7,12 @@ router.post("/login", function(req, res, next) {
   const username = req.body.username;
   const password = req.body.password;
 
+  console.log("LOGIN INTENT:", username);
+
   if (dataService.validateUser(username, password)) {
     req.session.isLogged = true;
     req.session.username = username;
+    console.log("SESSION USER:", req.session.username);
     res.redirect("/");
   } else {
     res.render("login", { error: "User does not exist" });
